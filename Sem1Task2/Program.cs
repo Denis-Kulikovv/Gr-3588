e4596e1da124f370a3f6d788eca5062e39c49479
Denis <dvkulikovv@mail.ru>
2022-12-02 14:51:32 +0300

    задача д/з на сравнение 2х чисел

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Просим ввести 2 числа
+Console.WriteLine("Введите два числа ");
+// Считываем данные с консоли
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+// Проверяем на пустые значения
+if(inputLineA!=null&&inputLineB!=null)
+{// Переводим в число
+    int num1 = int.Parse(inputLineA);
+    int num2 = int.Parse(inputLineB);
+// Сравниваем эти числа
+    if(num1>num2)
+// Выводим большее из них    
+    Console.WriteLine(num1);
+    
+    else
+    Console.WriteLine(num2);
+}
+
