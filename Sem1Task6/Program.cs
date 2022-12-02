6b72960de2d3e06e21044adcb677904bf49d7976
Denis <dvkulikovv@mail.ru>
2022-12-02 17:07:32 +0300

    д/з проверяем число на четность

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Просим ввести число
+Console.WriteLine("Введите число");
+// Считываем данные с консоли
+string? inputA = Console.ReadLine();
+// Проверяем на пустые значения
+if(inputA!=null)
+{// Переводим в число
+    int numA = int.Parse(inputA);
+// Вводим переменную и присваеваем ей значение остатка от деления на 2
+    int numB = numA%2;
+// Проверяем остаток и выводим данные
+    if(numB==0)
+    Console.WriteLine("ЧЕТНОЕ");
+    else
+    Console.WriteLine("НЕЧЕТНОЕ");
+    
+}
