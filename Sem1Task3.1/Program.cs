36c5e0a9433953a55708cbef5bcf8933d9a7fc32
Denis <dvkulikovv@mail.ru>
2022-12-02 16:31:09 +0300

    поиск дня недели

---
+++ Sem1Task3.1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+// Проверяем на пустоту
+if(inputLine!=null)
+{
+    string[] dayOfWeek = new string[7];
+    dayOfWeek[0] = "понедельник";
+    dayOfWeek[1] = "вторник";
+    dayOfWeek[2] = "среда";
+    dayOfWeek[3] = "четверг";
+    dayOfWeek[4] = "пятница";
+    dayOfWeek[5] = "суббота";
+    dayOfWeek[6] = "воскресенье";
+    // Переводим в число
+    int inputNumber = int.Parse(inputLine);
+    // 
+    string outDayOfWeek = dayOfWeek[inputNumber-1];
+    
+    Console.WriteLine(outDayOfWeek);
+} 
+
