0fe6beedf29046746e94153af125de552765f612
Denis <dvkulikovv@mail.ru>
2022-12-03 13:29:17 +0300

    Проверяем кратность 2 числа первому

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿int A = int.Parse(Console.ReadLine()??"0");
+int B = int.Parse(Console.ReadLine()??"0");
+int R = B%A;
+// Задаем условие равен ли остаток 0
+if(R==0)
+{
+   Console.WriteLine("Второе число делится на первое без остатка"); 
+} 
+
+else
+   Console.WriteLine("Второе число делится на первое c остаткoм " + R);
