1ae3cd0490160ff86001591e932f20619ad273be
Denis <dvkulikovv@mail.ru>
2022-12-03 12:36:21 +0300

    Находим случайное 2значное число и сравниваем 1 и 2

---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Создаем экземпляр класса рандомайзер
+System.Random numberSintezator = new System.Random();
+// Получаем новое случайное число
+int number = numberSintezator.Next(10,100);
+// Выводим данные в консоль
+Console.WriteLine(number);
+// Получаем первое и второе число по оттдельности
+int firstNumber = number/10;
+int secondNumber = number%10;
+// сравниваем числа
+if(firstNumber>secondNumber)
+
+{// Выводим данные в консоль
+    Console.WriteLine(firstNumber);
+}
+
+else
+
+{// Выводим данные в консоль
+    Console.WriteLine(secondNumber);
+}
