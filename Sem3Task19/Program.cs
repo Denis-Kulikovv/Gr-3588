096d318b97dcfd8566843a9d12d19f84515ee019
Denis <dvkulikovv@mail.ru>
2022-12-09 11:39:19 +0300

    д/з проверяем палиндром

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Метод читает данные от пользователя
+int numbers = ReadData("Введите пятизначное число");
+// Метод проверяет является ли число палиндромом 
+bool result = PalinTest(numbers);
+// Выводим результат в консоль
+Console.WriteLine(result);
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine("Введите пятизначное число");
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+// Метод проверяет является ли число палиндромом
+bool PalinTest(int numbers)
+{
+    bool result = false;
+    result = (numbers/10000==numbers%10&&(numbers/1000)%10==(numbers/10)%10);
+
+    return result;
+} 
+
+
+
