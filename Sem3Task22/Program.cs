87ad88fea6e42bf4d3f6fca48725d38cf53da930
Denis <dvkulikovv@mail.ru>
2022-12-09 14:58:35 +0300

    выводим квадрат чисел от одного до n

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿int n = ReadData ("Введите число: ");
+
+string line = LineBuilder(n);
+
+Console.WriteLine(line);
+
+
+
+// Метод читает данные от пользователя
+int ReadData(string msg) 
+{     
+    Console.WriteLine(msg);    
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+string LineBuilder(int n)
+{
+    string line = "";
+    string line2 = "";
+    for (int i=1;i<n;i++)
+    {
+        line+=i+",";
+        line2+=i*i+ ",";
+    }
+    line+= n;
+    line2+= n*n;
+    return line + "\n"+ line2;
+}
+
