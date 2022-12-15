59af90088c4fd5008cd976e2c05b15811a8a32cb
Denis <dvkulikovv@mail.ru>
2022-12-15 19:19:32 +0300

    Д/з Возводим число А в степеньВ

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int ReadData(string msg) // Просим ввести число
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)// Метод выводит результат
+{
+    Console.WriteLine(res);
+}
+
+int Pow(int A, int B) // Метод вычисляет число А в степени Б
+{
+    int res = 1;
+    for(int i=1;i<=B;i++)
+    {
+        res*=A;
+    }
+    return res;
+}   
+
+int A = ReadData("Введите число A");
+int B = ReadData("Введите число B");
+
+
+int res = Pow(A,B);
+
+PrintData("Число " + A + " в степени " + B + " равно : " + res);
