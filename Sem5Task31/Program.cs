b88325151a47740e66c9812120051556985fb833
Denis <dvkulikovv@mail.ru>
2022-12-15 19:26:14 +0300

    Сумма положительных и отриц чисел в массиве

---
+++ Sem5Task31/Program.cs
@@ -0,0 +1,42 @@
+﻿// S
+
+int positivsum = 0;
+int negativsum = 0;
+
+int[] testArr=GenArr(12, -9, 9);
+PrintData("Cумма положительных чисел в масстве равна ", positivsum);
+PrintData("Cумма отрицательных чисел в масстве равна ", negativsum);
+NegativPositivSum(testArr);
+
+int[] GenArr(int len, int minValue, int maxValue) 
+{   Random rnd = new Random();
+    int[] arr = new int[len];
+    for(int i=0;i<arr.Length;i++)
+    {
+        arr[i]=new Random().Next(minValue,maxValue+1);
+    }
+    return arr;
+}
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res+value);
+
+}
+
+void NegativPositivSum(int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {  
+        if(arr[i]>0)
+        {
+            positivsum+=arr[i];
+        }
+        else
+        {
+            negativsum+=arr[i];
+        }
+       
+    }
+    
+
+}
